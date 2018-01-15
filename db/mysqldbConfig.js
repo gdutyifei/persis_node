27eb8f7c76c992f275680c55fb2e711f2c281e20
罗奕斐 <gdutyifei@163.com>
2018-01-15 16:18:58 +0800

    新增mysql依赖

---
+++ db/mysqldbConfig.js
@@ -0,0 +1,14 @@
+/**
+ * Created by luoyifei on 2018/1/15.
+ */
+
+// 数据库连接配置
+module.exports = {
+    mysql: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'persis',
+        port: 3306
+    }
+}
