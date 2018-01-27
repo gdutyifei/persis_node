c7780a0c1fc4e72b143331fa26ea54d5c75a8b07
罗奕斐 <gdutyifei@163.com>
2018-01-27 15:38:38 +0800

    修改表结构， 做数据冗余处理

--- sql/Activities.sql
+++ sql/Activities.sql
@@ -1,4 +1,5 @@
 USE persis;
+DROP TABLE IF EXISTS `Activities`;
 CREATE TABLE `Activities` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
 	`created_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
@@ -7,7 +8,7 @@ CREATE TABLE `Activities` (
 	`updated_date` DATETIME NULL DEFAULT NULL,
 	`activity_date` VARCHAR(10) NULL DEFAULT NULL COMMENT '活动时间' COLLATE 'utf8mb4_unicode_ci',
 	`period` VARCHAR(10) NULL DEFAULT NULL COMMENT '活动期数' COLLATE 'utf8mb4_unicode_ci',
-	`book_ids` mediumtext COLLATE utf8mb4_unicode_ci COMMENT '书籍ids',
+	`book_infos` mediumtext COLLATE utf8mb4_unicode_ci COMMENT '书籍ids',
 	PRIMARY KEY (`id`)
 )
 COLLATE='utf8mb4_bin'
--- sql/Participation.sql
+++ sql/Participation.sql
@@ -1,13 +1,14 @@
 USE persis;
+DROP TABLE IF EXISTS `Participation`;
 CREATE TABLE `Participation` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
 	`created_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
 	`created_date` DATETIME NULL DEFAULT NULL,
 	`updated_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
 	`updated_date` DATETIME NULL DEFAULT NULL,
-	`user_id` BIGINT(20) NULL DEFAULT NULL COMMENT '用户id' COLLATE 'utf8mb4_unicode_ci',
+	`user_info` mediumtext COLLATE utf8mb4_unicode_ci COMMENT '用户信息',
 	`book_id` BIGINT(20) NULL DEFAULT NULL COMMENT '书籍id' COLLATE 'utf8mb4_unicode_ci',
-	`period` VARCHAR(10) NULL DEFAULT NULL COMMENT '活动期数' COLLATE 'utf8mb4_unicode_ci',
+	`activity_id` VARCHAR(10) NULL DEFAULT NULL COMMENT '活动id' COLLATE 'utf8mb4_unicode_ci',
 	PRIMARY KEY (`id`)
 )
 COLLATE='utf8mb4_bin'
