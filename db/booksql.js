8c19e5fb14559a55acd3d80b99e79bb2e53291c5
罗奕斐 <gdutyifei@163.com>
2018-01-29 10:24:36 +0800

    数据冗余操作，sql修改

--- db/booksql.js
+++ db/booksql.js
@@ -4,7 +4,7 @@
 var booksSql = {
     insert: 'INSERT INTO Books(book_name, cover_url, created_date, created_by, updated_date, updated_by) VALUES(?, ?, NOW(), "system", NOW(), "system")',
     queryAll: 'SELECT * FROM Books',
-    selectByBookId: 'SELECT * FROM Books WHERE id = ?'
+    selectByBookId: 'SELECT id, book_name, cover_url FROM Books WHERE id = ?'
 };
 
 module.exports = booksSql;
