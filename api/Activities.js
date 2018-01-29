81fd101827ed45453a26876e3c4e7a5367c5ddd3
罗奕斐 <gdutyifei@163.com>
2018-01-29 11:06:42 +0800

    删除无用依赖

--- api/Activities.js
+++ api/Activities.js
@@ -10,7 +10,6 @@ var dbConfig = require('../db/mysqldbConfig');
 var activitySql = require('../db/activitySql');
 var bookSql = require('../db/booksql');
 
-var utils = require('../common/utils');
 // 使用mysqldbConfig.js的配置信息创建一个MYSQL连接池
 var pool = mysql.createPool(dbConfig.mysql);
 // 响应一个JSON数据
