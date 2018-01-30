2505c2e0836a2668c7a823875635e2dc8af06705
罗奕斐 <gdutyifei@163.com>
2018-01-30 10:49:43 +0800

    修改sql

--- db/activitySql.js
+++ db/activitySql.js
@@ -2,8 +2,8 @@
  * Created by luoyifei on 2018/1/20.
  */
 var activitySql = {
-    insert: 'INSERT INTO Activities(activity_date, period, book_infos, created_date, created_by, updated_date, updated_by) VALUES(?, ?, ?, NOW(), "system", NOW(), "system");',
-    selectInfoByDate: 'SELECT a.period, a.activity_date, a.book_ids FROM Activities a WHERE a.activity_date = ?'
+    insert: 'INSERT INTO Activities(activity_date, period, book_ids, book_infos, created_date, created_by, updated_date, updated_by) VALUES(?, ?, ?, ?, NOW(), "system", NOW(), "system");',
+    selectInfoByDate: 'SELECT a.id, a.period, a.activity_date, a.book_ids, a.book_infos FROM Activities a WHERE a.activity_date = ? LIMIT 1'
 }
 
 module.exports = activitySql;
