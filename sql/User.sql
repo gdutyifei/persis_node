ef7fae407af962b77b0b62c47eb92b6eda98e277
罗奕斐 <gdutyifei@163.com>
2018-02-04 12:18:06 +0800

    增加接口

--- sql/User.sql
+++ sql/User.sql
@@ -1,4 +1,5 @@
 USE persis;
+DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
 	`created_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
@@ -7,7 +8,7 @@ CREATE TABLE `User` (
 	`updated_date` DATETIME NULL DEFAULT NULL,
 	`openid` VARCHAR(50) NULL DEFAULT NULL COMMENT '微信openid' COLLATE 'utf8mb4_unicode_ci',
 	`union_id` VARCHAR(50) NULL DEFAULT NULL COMMENT '微信unionId' COLLATE 'utf8mb4_unicode_ci',
-	`avatar_url` VARCHAR(80) NULL DEFAULT NULL COMMENT '头像url' COLLATE 'utf8mb4_unicode_ci',
+	`avatar_url` VARCHAR(200) NULL DEFAULT NULL COMMENT '头像url' COLLATE 'utf8mb4_unicode_ci',
 	`nick_name` VARCHAR(20) NULL DEFAULT NULL COMMENT '微信昵称' COLLATE 'utf8mb4_unicode_ci',
 	`sex` VARCHAR(10) NULL DEFAULT NULL COMMENT '微信性别' COLLATE 'utf8mb4_unicode_ci',
 	PRIMARY KEY (`id`)
