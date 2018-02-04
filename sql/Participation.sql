0667360d33a56c3573a44d801e538a105a9d51d1
罗奕斐 <gdutyifei@163.com>
2018-02-04 10:41:25 +0800

    增加userId字段用于排序

--- sql/Participation.sql
+++ sql/Participation.sql
@@ -6,6 +6,7 @@ CREATE TABLE `Participation` (
 	`created_date` DATETIME NULL DEFAULT NULL,
 	`updated_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
 	`updated_date` DATETIME NULL DEFAULT NULL,
+	`user_id` BIGINT(20) NULL DEFAULT NULL COMMENT '用户id' COLLATE 'utf8mb4_unicode_ci',
 	`user_info` mediumtext COLLATE utf8mb4_unicode_ci COMMENT '用户信息',
 	`book_id` BIGINT(20) NULL DEFAULT NULL COMMENT '书籍id' COLLATE 'utf8mb4_unicode_ci',
 	`activity_id` VARCHAR(10) NULL DEFAULT NULL COMMENT '活动id' COLLATE 'utf8mb4_unicode_ci',
