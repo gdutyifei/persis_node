5cc53e5317b41e8d886edba0dc57d69049fed2d2
罗奕斐 <gdutyifei@163.com>
2018-02-04 12:17:10 +0800

    用户头像字段增长

--- db/usersql.js
+++ db/usersql.js
@@ -5,7 +5,7 @@ var usersSql = {
     insert: 'INSERT INTO User(openid, union_id, avatar_url, nick_name, sex, created_date, created_by, updated_date, updated_by) VALUES(?, ?, ?, ?, ?, NOW(), "system", NOW(), "system")',
     queryAll: 'SELECT * FROM User',
     getUserById: 'SELECT * FROM User WHERE id = ? ',
-    getUserByOpenid: 'SELECT id, union_id, avatar_url, nick_name, sex FROM User WHERE openid = ?'
+    getUserByOpenid: 'SELECT id, openid, union_id, avatar_url, nick_name, sex FROM User WHERE openid = ?'
 };
 
 module.exports = usersSql;
